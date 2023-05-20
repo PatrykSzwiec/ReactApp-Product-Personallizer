ce0ee1bdbc1dcf997ef829b7f1174ec1005b2f13
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-05-20 11:47:56 +0200

    Change Products component to show all data from data/Products, also add title and BasePrice at Product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,23 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 
 const Product = props => {
+  const { title, basePrice } = props; // Destructure the props
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
+        <img
           className={styles.image}
           alt="Kodilla shirt"
           src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
